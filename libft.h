86bcecde22538d07ed6f4d66724d2246e4a0cc88
fschmid <fschmid@student.42heilbronn.de>
2022-10-11 15:02:26 +0200

    added ft_strnstr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 09:17:52 by fschmid           #+#    #+#             */
-/*   Updated: 2022/10/11 14:41:53 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/10/11 14:50:39 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,4 +52,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+char	*ft_strnstr(const char *src, const char *str, size_t len);
 #endif
