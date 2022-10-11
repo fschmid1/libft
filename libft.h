232a64b6a3aa4ac78cf7bfe8d56c9ddfb5d44c48
fschmid <fschmid@student.42heilbronn.de>
2022-10-11 14:45:38 +0200

    added ft_memcmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 09:17:52 by fschmid           #+#    #+#             */
-/*   Updated: 2022/10/11 14:18:47 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/10/11 14:41:53 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,4 +50,6 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 void	*ft_memchr(const void *s, int c, size_t n);
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
