2572a2336cb709c9fc1caaa6211090d7cbb669a9
fschmid <fschmid@student.42heilbronn.de>
2022-10-11 12:33:17 +0200

    updated memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 12:17:01 by fschmid           #+#    #+#             */
-/*   Updated: 2022/10/11 12:20:33 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/10/11 12:33:11 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 	size_t	i;
 
 	if (!dst && !src)
-		return (0);
+		return (NULL);
 	i = 0;
 	while (i < n)
 	{
