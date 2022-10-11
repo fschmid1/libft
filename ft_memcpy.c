5a2c599ba08596babd1f82e07042a24cfd056907
fschmid <fschmid@student.42heilbronn.de>
2022-10-11 12:21:50 +0200

    added ft_memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 12:17:01 by fschmid           #+#    #+#             */
+/*   Updated: 2022/10/11 12:20:33 by fschmid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	size_t	i;
+
+	if (!dst && !src)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 09:17:52 by fschmid           #+#    #+#             */
-/*   Updated: 2022/10/11 12:05:35 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/10/11 12:18:15 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ int		ft_strlen(char *str);
 void	*ft_memset(void *b, int c, size_t len);
 
 void	ft_bzero(void *s, size_t n);
+
+void 	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 
 void	*ft_memmove(void *dst, const void *src, size_t len);
 #endif
