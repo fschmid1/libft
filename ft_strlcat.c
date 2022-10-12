743ddaac0a8daca88bde3a424a7ca5af27d83ef8
fschmid <fschmid@student.42heilbronn.de>
2022-10-12 10:58:27 +0200

    redone strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:04:37 by fschmid           #+#    #+#             */
-/*   Updated: 2022/10/11 13:12:33 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/10/12 10:57:46 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,26 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
+	size_t	res;
+	size_t	dst_len;
+	size_t	src_len;
+	char	*s;
 
+	s = (char *) src;
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(s);
 	i = 0;
-	if (dstsize == 0)
+	res = 0;
+	if (dstsize > dst_len)
+		res = src_len + dst_len;
+	else
+		res = src_len + dstsize;
+	while (s[i] && (dst_len + 1) < dstsize)
 	{
-		while (src[i])
-			i++;
-		return (i);
-	}
-	while (i < dstsize - 1 && src[i] != '\0')
-	{
-		dst[i] = src[i];
+		dst[dst_len] = s[i];
+		dst_len++;
 		i++;
 	}
-	if (i < dstsize)
-		dst[i] = '\0';
-	while (src[i] != '\0')
-		i++;
-	return (i);
+	dst[dst_len] = '\0';
+	return (res);
 }
