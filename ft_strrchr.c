e4957de0bff1200636e1051e9167e4ff12f0c3c5
fschmid <fschmid@student.42heilbronn.de>
2022-10-12 10:40:34 +0200

    fixed strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:23:14 by fschmid           #+#    #+#             */
-/*   Updated: 2022/10/11 14:06:36 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/10/12 10:39:54 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,20 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	s += ft_strlen((char *) s) - 1;
-	while (*s != '\0')
+	char	*last;
+	char	find;
+	size_t	i;
+
+	last = (char *)s;
+	find = (char)c;
+	i = ft_strlen((char *) s);
+	while (i > 0)
 	{
-		if (*s == c)
-			return ((char *)s);
-		s--;
+		if (last[i] == find)
+			return (last + i);
+		i--;
 	}
+	if (last[i] == find)
+		return (last);
 	return (0);
 }
