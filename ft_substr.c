aa337584bb238de25476e5e74759c5058da77432
fschmid <fschmid@student.42heilbronn.de>
2022-10-13 11:42:23 +0200

    fixed substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 11:02:47 by fschmid           #+#    #+#             */
-/*   Updated: 2022/10/13 10:51:42 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/10/13 11:41:24 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t		i;
 	size_t		k;
 
-	src = (char *) s;
-	dest = (char *) malloc(sizeof(char) * (len + 1));
+	dest = (char *) ft_calloc(1, sizeof(char) * (len + 1));
 	if (!dest)
 		return (0);
 	i = 0;
-	k = start;
-	while (src[i + k] != '\0' && i < len)
+	if (start < ((unsigned int) ft_strlen((char *) s)))
 	{
-		dest[i] = src[i + k];
-		i++;
+		src = (char *) s;
+		k = start;
+		while (src[i + k] != '\0' && i < len)
+		{
+			dest[i] = src[i + k];
+			i++;
+		}
 	}
-	dest[i] = '\0';
 	return (dest);
 }
