aa284bb07cdbe977650848efba0a059747267286
fschmid <fschmid@student.42heilbronn.de>
2022-10-13 13:53:54 +0200

    fixed strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 13:04:37 by fschmid           #+#    #+#             */
-/*   Updated: 2022/10/12 10:57:46 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/10/13 13:53:39 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,22 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
-	size_t	res;
-	size_t	dst_len;
-	size_t	src_len;
-	char	*s;
+	size_t	j;
+	size_t	srclen;
+	size_t	dstlen;
 
-	s = (char *) src;
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(s);
 	i = 0;
-	res = 0;
-	if (dstsize > dst_len)
-		res = src_len + dst_len;
-	else
-		res = src_len + dstsize;
-	while (s[i] && (dst_len + 1) < dstsize)
+	srclen = ft_strlen((char *) src);*
+	dstlen = ft_strlen(dst);
+	j = dstlen;
+	if (dstlen > dstsize)
+		return (srclen + dstsize);
+	while (src[i] != 0 && dstlen + i + 1 < dstsize)
 	{
-		dst[dst_len] = s[i];
-		dst_len++;
+		dst[j] = src[i];
 		i++;
+		j++;
 	}
-	dst[dst_len] = '\0';
-	return (res);
+	dst[j] = 0;
+	return (dstlen + srclen);
 }
