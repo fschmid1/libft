b970212041cb87b6ea901c2609e49f8f64b22074
fschmid <fschmid@student.42heilbronn.de>
2022-10-13 14:01:23 +0200

    fixed strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 14:07:25 by fschmid           #+#    #+#             */
-/*   Updated: 2022/10/12 10:47:05 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/10/13 14:00:33 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	while (s1[i] != '\0' && s1[i] == s2[i] && i < n - 1)
 		i++;
-	return ((char)s1[i] - (char)s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
