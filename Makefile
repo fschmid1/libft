21a1da8ae794489f8e943ffea9ca03021b3ae948
fschmid <fschmid@student.42heilbronn.de>
2022-11-16 14:39:46 +0100

    added next line

--- Makefile
+++ Makefile
@@ -41,7 +41,8 @@ MANDO = ft_isalpha.c \
 		ft_putchar_fd.c \
 		ft_putstr_fd.c \
 		ft_putendl_fd.c \
-		ft_putnbr_fd.c
+		ft_putnbr_fd.c \
+		get_next_line.c
 
 BONUS = ft_lstnew.c \
 		ft_lstadd_front.c \
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fschmid <fschmid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 09:17:52 by fschmid           #+#    #+#             */
-/*   Updated: 2022/10/20 16:23:20 by fschmid          ###   ########.fr       */
+/*   Updated: 2022/11/16 14:39:22 by fschmid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,4 +108,10 @@ void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+char	*get_next_line(int fd);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
 #endif
